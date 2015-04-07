8a9fa2ddae255f12e6e47dcfc243aee1e59d2ef8
Andrew Sullivan <andrew@practical-admin.com>
2015-04-07 15:23:08 -0400

    Initial import.  Updated to use correct PoSh verbs.

---
+++ powershell/Other/Get-GroupsForObject.ps1
@@ -0,0 +1,34 @@
+function Get-GroupsForObject {
+    [cmdletbinding()]
+    param(
+        [string]$Object = "", 
+        [int]$Level = 0
+    )
+ 
+    $indent = "-" * $Level
+ 
+    $d = Get-ADObject -Identity $Object -Properties SamAccountName
+ 
+    if ($Level -eq 0) {
+        Write-Host "$indent# $($d.SamAccountName)"
+    }
+ 
+    if ($d.ObjectClass -eq "user" -and $Level -eq 0) {
+        $e = Get-ADUser -Identity $d.DistinguishedName -Properties MemberOf
+ 
+    } elseif ($d.ObjectClass -eq "group") {
+        if ($Level -gt 0) {
+            Write-Host "$indent-> $($d.SamAccountName)"
+        }
+ 
+        $e = Get-ADGroup -Identity $d.DistinguishedName -Properties MemberOf
+ 
+    }
+ 
+    $e.MemberOf | Sort-Object | %{
+        # prevent a loop if the group is a member of itself
+        if ( $_ -ne $e.DistinguishedName ) {
+            Get-GroupsForObject -Object $_  -Level($Level + 1)
+        }
+    }
+}
